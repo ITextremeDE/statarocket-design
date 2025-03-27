a9db2fac7412a04b63efa279aa8e08efcfbe107b
Jürgen Schadek <56023985+Juergen-Schadek@users.noreply.github.com>
2025-03-27 10:13:58 +0100

    Dark Mode added

--- src/main.js
+++ src/main.js
@@ -1,2 +1,31 @@
 // Tailwind CSS
 import "./tailwind.css";
+
+// Alpine.js
+import Alpine from "alpinejs";
+
+window.htmlData = function () {
+    return {
+        themePreference: localStorage.getItem("theme") || "system",
+        systemTheme: window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "light",
+        setSystemTheme(theme) {
+            this.systemTheme = theme;
+        },
+        setThemePreference(theme) {
+            this.themePreference = theme;
+            localStorage.setItem("theme", theme);
+        },
+        get darkMode() {
+            return (
+                this.themePreference === "dark" ||
+                (this.themePreference === "system" &&
+                    this.systemTheme === "dark")
+            );
+        },
+    };
+};
+
+window.Alpine = Alpine;
+Alpine.start();
